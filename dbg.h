3fb75a5a97b517f53f4bcd86500eb6205ed9a6a2
Scott Cheloha <scottcheloha@gmail.com>
2015-09-13 21:43:00 -0400

    Changes some macros to simply exit(1) instead of JMPing to an 'error' label.  Added declaration of program name.  Added function prototype for setting program name from main() in needleman-wunsch.c

--- dbg.h
+++ dbg.h
@@ -1,55 +1,92 @@
-/* dbg.h, Copyright (c) 2013-2015 Zed Shaw */
+/*-
+Copyright (c) 2010, Zed A. Shaw.  All rights reserved.
+Copyright (c) 2015, Scott S. Cheloha.  All rights reserved.
 
-/* dbg.h - Zed's Awesome Debug Macros
-           (see http://c.learncodethehardway.org/book/ex20.html)
- */
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in
+      the documentation and/or other materials provided with the
+      distribution.
+
+    * Neither the name of the Learn C The Hard Way, Zed A. Shaw, nor the
+      names of its contributors may be used to endorse or promote
+      products derived from this software without specific prior written
+      permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+-*/
 
 #ifndef __dbg_h__
 #define __dbg_h__
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+
+/* Global program name for logging */
+char *prog;
+
+/* Set the global program name */
+void set_prog_name(char *name);
+
 
 #ifdef NDEBUG
 #define debug(M, ...)
 #else
-#define debug(M, ...)                                   \
-        fprintf(stderr,                                 \
-                "[DEBUG] %s:%d: " M "\n",               \
-                __FILE__, __LINE__, ##__VA_ARGS__)
+#define debug(M, ...)                                      \
+        fprintf(stderr,                                    \
+                "%s: debug: %s:%d: " M "\n",               \
+                prog, __FILE__, __LINE__, ##__VA_ARGS__)
 #endif
 
-#define clean_errno() (errno == 0 ? "None" : strerror(errno))
+/* Print a single backspace to remove the space left for the possible
+   errno string */
+#define clean_errno() (errno == 0 ? "\b" : strerror(errno))
 
 #define log_err(M, ...)                                                 \
         fprintf(stderr,                                                 \
-                "[ERROR] (%s:%d: errno: %s) " M "\n",                   \
-                __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+                "%s: error: %s:%d: %s " M "\n",                        \
+                prog, __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 
 #define log_warn(M, ...)                                                \
         fprintf(stderr,                                                 \
-                "[WARN] (%s:%d: errno: %s) " M "\n",                    \
-                __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+                "%s: warning: %s:%d: %s " M "\n",                       \
+                prog, __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 
-#define log_info(M, ...)                                \
-        fprintf(stderr,                                 \
-                "[INFO] (%s:%d) " M "\n",               \
-                __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_info(M, ...)                                   \
+        fprintf(stderr,                                    \
+                "%s: " M "\n",                             \
+                prog, ##__VA_ARGS__)
 
-#define check(A, M, ...) \
+#define check(A, M, ...)                        \
         if (!(A)) {                             \
                 log_err(M, ##__VA_ARGS__);      \
-                errno=0;                        \
-                goto error;                     \
+                exit(1);                        \
         }
 
 #define sentinel(M, ...)                        \
-        {                                       \
                 log_err(M, ##__VA_ARGS__);      \
-                errno=0;                        \
-                goto error;                     \
-        }
+                exit(1);
+
+#define unreachable(M, ...)                                     \
+        sentinel("Unreachable code reached; giving up.")
 
 #define check_mem(A) check((A), "Out of memory.")
 
