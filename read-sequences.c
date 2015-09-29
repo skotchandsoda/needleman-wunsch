8d8ec4778f3331964e1789e0673f714c4dc28313
Scott Cheloha <scottcheloha@gmail.com>
2015-09-28 23:30:14 -0400

    Fixed bug in input string length computation.

--- read-sequences.c
+++ read-sequences.c
@@ -1,36 +1,35 @@
-/*-
-Copyright (c) 2015, Scott Cheloha.
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are
-met:
-
-1. Redistributions of source code must retain the above copyright
-   notice, this list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in the
-   documentation and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its
-   contributors may be used to endorse or promote products derived from
-   this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
--*/
-
-/* read-sequences.c - Read sequences from a stream into memory. */
+/* read-sequences.c - Read sequences from a stream into memory.
+ *
+ * Copyright (c) 2015, Scott Cheloha.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ *   3. Neither the name of the copyright holder nor the names of its
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include <ctype.h>
 #include <errno.h>
@@ -61,14 +60,10 @@ read_sequence_from_stream(FILE *in, int eof_ok)
         int seq_max = INPUT_STRING_BUF_SIZE;
         char *seq = (char *)malloc(seq_max * sizeof(char));
 
-        /* Read characters from stdin into string s until we hit whitespace */
-        while (EOF != (c = fgetc(in))) {
-                if (isspace(c)) {
-                        break;
-                } else {
-                        seq[i] = (char)c;
-                        i = i + 1;
-                }
+        /* Read characters from stdin into seq until we hit whitespace */
+        while (EOF != (c = fgetc(in)) && !isspace(c)) {
+                seq[i] = (char)c;
+                i = i + 1;
 
                 /* If we're out of room, allocate more space */
                 if (seq_max == i) {
@@ -81,8 +76,9 @@ read_sequence_from_stream(FILE *in, int eof_ok)
         /* Make sure we didn't get an error or find EOF prematurely */
         check_fgetc_err_and_eof(in, eof_ok);
 
-        /* Null-terminate the input string by hand */
-        seq[i+1] = '\0';
+        /* Null-terminate the input string by hand.  Note that i is the
+         * last index we never wrote a character to */
+        seq[i] = '\0';
 
         return seq;
 }
